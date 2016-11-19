af52c3ed3b933e0418df266e4a7beb1e991f71c3
RonjaO <ronja.oja@gmail.com>
2016-11-19 14:29:25 +0200

    JavaScript-muutos

--- js/salaisuus.js
+++ js/salaisuus.js
@@ -1,4 +1,12 @@
 function salaisuus() {
-    document.getElementById("salaisuus").style = "backround-color:#90EE90";
+    var div = document.getElementById("salaisuus");
+    div.style.removeProperty("position")
+    div.style.removeProperty("left");
+    div.style.removeProperty("top");
+    div.style.removeProperty("width");
+    div.style.removeProperty("height");
+    div.style.removeProperty("overflow");
+    
+    div.style.backgroundColor = "#90EE90";
 }
 
