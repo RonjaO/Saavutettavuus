df937279125851add92dd0ef1dafa94b9d87b5c8
RonjaO <ronja.oja@gmail.com>
2016-11-19 16:36:31 +0200

    JS muutos

--- js/salaisuus.js
+++ js/salaisuus.js
@@ -3,6 +3,13 @@ $(document).ready(function() {
     $("#painike").bind("click", function(event) {
         $("body").append("<p>Lisätty teksti</p>");
         alert("fooo");
+        var div = document.getElementById("salaisuus");
+        div.style.removeProperty("position")
+        div.style.removeProperty("width");
+        div.style.removeProperty("height");
+        div.style.removeProperty("opacity");
+
+        div.style.backgroundColor = "#90EE90";
     });
 });
 
