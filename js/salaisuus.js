dbdc7240fd2f60896fb0961d60ac9bc53512cd0b
RonjaO <ronja.oja@gmail.com>
2016-11-19 15:44:09 +0200

    JQuery-testi

--- js/salaisuus.js
+++ js/salaisuus.js
@@ -1,17 +1,22 @@
-alert("jabajee");
+$(document).ready(function() {
+    $("#painike").click(function(event) {
+        alert("fooo");
+    });
+});
 
-function salaisuus() {
-    alert("foo");
-    var div = document.getElementById("salaisuus");
-    div.style.removeProperty("position")
-    div.style.removeProperty("left");
-    div.style.removeProperty("top");
-    div.style.removeProperty("width");
-    div.style.removeProperty("height");
-    div.style.removeProperty("overflow");
-    
-    div.style.backgroundColor = "#90EE90";
-    
-    alert(div.style.position);
-}
+
+// function salaisuus() {
+//     alert("foo");
+//     var div = document.getElementById("salaisuus");
+//     div.style.removeProperty("position")
+//     div.style.removeProperty("left");
+//     div.style.removeProperty("top");
+//     div.style.removeProperty("width");
+//     div.style.removeProperty("height");
+//     div.style.removeProperty("overflow");
+//
+//     div.style.backgroundColor = "#90EE90";
+//
+//     alert(div.style.position);
+// }
 
