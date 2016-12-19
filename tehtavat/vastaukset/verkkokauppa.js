c9a9ded8225928f0c6f429cd5aaf33b73579f55a
RonjaO <ronja.oja@gmail.com>
2016-12-19 20:44:18 +0200

    Malllivastaukset

--- tehtavat/vastaukset/verkkokauppa.js
+++ tehtavat/vastaukset/verkkokauppa.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+    // Etsitään kaikki elementit, joilla on luokka 'alimenulinkki'
+    // lisätään niille indeksin mukainen id sekä tapahtumankuuntelija
     $(".alimenulinkki").each(function(index) {
         $(this).attr("id", index);
         $(this).click(function(eventInformation) {
@@ -10,10 +12,15 @@ $(document).ready(function(){
 });
 
 function showDropdown(index) {
+    // Index on alimenun avaavan linkin id.
+    // Haetaan sivulta kaikki elementit, joiden luokka on 'alimenu-sisalto'
+    // Poistetaan ensin kaikilta luokka 'show'
+    // Etsitään indexillä oikea alimenu-sisalto ja lisätään sille luokka 'show'
     $(".alimenu-sisalto").removeClass("show");
     $(".alimenu-sisalto:eq(" + index + ")").addClass("show");
 }
 
+// Jos käyttäjä klikkaa jotain muuta kohdetta näytöllä, piilotetaan alimenut
 window.onclick = function(event) {
     if (!event.target.matches('.alimenulinkki')) {
        $(".alimenu-sisalto").removeClass("show");
