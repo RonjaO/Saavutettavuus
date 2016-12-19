1cbbd0ae7f6506cf6606e6868bd584cc95300038
RonjaO <ronja.oja@gmail.com>
2016-12-19 16:33:40 +0200

    Mallivastausjuttuja

--- tehtavat/vastaukset/verkkokauppa.js
+++ tehtavat/vastaukset/verkkokauppa.js
@@ -1,3 +1,26 @@
+$(document).ready(function(){
+
+    $("header nav a").each(function(index) {
+        $(this).attr("id", index);
+        $(this).click(function(eventInformation) {
+            displayArticle(index);
+            eventInformation.preventDefault();
+        });
+    });
+});
+
+function displayArticle(index) {
+    $("article").addClass("hidden");
+    $("article:eq(" + index + ")").removeClass("hidden");
+}
+
+
+
+
+
 function avaa() {
     document.getElementById("kotimenu-sisalto").classList.toggle("show");
 }
+
+
+
