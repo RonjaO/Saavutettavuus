9a4c040e2af46d903f89bbb8ca71e4f7450691af
RonjaO <ronja.oja@gmail.com>
2016-12-19 13:50:23 +0200

    Mallivastaussivu

--- kokoa.sh
+++ kokoa.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for pala in index johdanto perushtml syvemmalle sisalto kaytettavyys; do
+for pala in index johdanto perushtml syvemmalle sisalto kaytettavyys mallivastaukset; do
     cat alku.part luennot/$pala.part > $pala.html
 done
