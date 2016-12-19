5efc64382d26db7b895441beea264abcdd8def35
RonjaO <ronja.oja@gmail.com>
2016-12-19 19:33:03 +0200

    Verkkokauppaan muutoksia

--- tehtavat/vastaukset/verkkokauppa.js
+++ tehtavat/vastaukset/verkkokauppa.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
 
-    $("header nav a").each(function(index) {
+    $(".alimenu").each(function(index) {
         $(this).attr("id", index);
         $(this).click(function(eventInformation) {
-            displayArticle(index);
+            showChildDropdown(index);
             eventInformation.preventDefault();
         });
     });
 });
 
-function displayArticle(index) {
-    $("article").addClass("hidden");
-    $("article:eq(" + index + ")").removeClass("hidden");
+function showChildDropdown(index) {
+    $("div.alimenu > div").addClass("hidden");
+    $("div#" + index + " > div").removeClass("hidden");
 }
 
 
 
 
 
-function avaa() {
-    document.getElementById("kotimenu-sisalto").classList.toggle("show");
-}
+//function avaa() {
+//    document.getElementById("kotimenu-sisalto").classList.toggle("show");
+//}
 
 
 
