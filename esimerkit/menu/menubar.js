399e0c12047a6bf32fb91138a2d24b45f71badad
RonjaO <ronja.oja@gmail.com>
2017-06-20 21:57:41 +0300

    Menubar-javascriptiä

---
+++ esimerkit/menu/menubar.js
@@ -0,0 +1,84 @@
+var keyCode = Object.freeze([
+    'SPACE': 32,
+    'ENTER': 13,
+    'DOWN': 40
+]);
+
+function setEventListeners(menubar) {
+    var element = menubar.firstElementChild;
+    
+    while (element) {
+        var menuItem = element.firstElementChild;
+        
+        if (menuItem.tagName === 'span') {
+            menuItem.addEventListener('keyDown', handleKeyDown(event, menuItem));
+            menuItem.addEventListener('click', handleClick(event, menuItem));
+            menuItem.addEventListener('mouseover', openPopupMenu(menuItem));
+            menuItem.addEventListener('mouseout', closePopupMenu(menuItem));
+            
+        }
+        
+        element = element.firstElementChild;
+    }
+    
+    console.log('All event listeners has set');
+}
+
+function closeAll(menubar) {
+    var element = menubar.firstElementChild;
+    
+    while (element) {
+        var menuItem = element.firstElementChild;
+        closePopupMenu(menuItem);
+        
+        element = nextElementSibling;
+    }
+}
+
+function closePopupMenu(menuItem) {
+    var popupMenu = menuItem.nextElementSibling;
+
+    popupMenu.style.display = 'none';
+    
+    menuItem.setAttribute('aria-expanded', 'false');
+    
+}
+
+function openPopUpMenu(menuItem) {
+    var popupMenu = menuItem.nextElementSibling;
+    
+    popupMenu.style.display = 'block';
+    popupMenu.style.position = 'absolute';
+    popupMenu.style.top = (rect.height - 1) + 'px';
+    popupMenu.style.zIndex = 100;
+    
+    menuItem.setAttribute('aria-expanded', 'true');
+}
+
+function handleKeydown(event, menuItem) {
+    switch(event.keyCode) {
+        case keyCode.SPACE:
+        case keyCode.ENTER:
+        case keyCode.DOWN:
+            if (menuItem.getAttribute('aria-expanded' === 'false')) {
+                openPopupMenu(menuItem);
+            } else {
+                closePopupMenu(menuItem);
+            }
+            break;
+        
+        default:
+            break;
+    }
+    
+}
+
+function handleClick(event, menuItem) {
+    if (menuItem.getAttribute('aria-expanded' === 'false')) {
+        openPopupMenu(menuItem);
+    } else {
+        closePopupMenu(menuItem);
+    }
+    
+}
+
